f4b8380b54cb69c1ab2e32f8a325b1837d600155
jamescross813 <crossjames813@gmail.com>
2023-09-18 22:23:21 -0500

    setup

---
+++ count_x.py
@@ -0,0 +1,7 @@
+# Write your count_char_x function here:
+
+# Uncomment these function calls to test your tip function:
+#print(count_char_x("mississippi", "s"))
+# should print 4
+#print(count_char_x("mississippi", "m"))
+# should print 1
