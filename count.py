84543d6ca480a1c76086698be09880dc723f1e01
jamescross813 <crossjames813@gmail.com>
2023-09-18 22:20:44 -0500

    start for loop

--- count.py
+++ count.py
@@ -1,7 +1,11 @@
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 # Write your unique_english_letters function here:
-def unique_english_letters(word):
+def unique_english_letters(input_word):
     count = 0
+    for let in letters:
+        pass
+
+            
 # Uncomment these function calls to test your function:
 #print(unique_english_letters("mississippi"))
 # should print 4
