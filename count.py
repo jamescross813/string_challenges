9dded9a4bad10be08acdd4e6b133cf365edf99aa
jamescross813 <crossjames813@gmail.com>
2023-09-18 21:59:53 -0500

    setup

---
+++ count.py
@@ -0,0 +1,8 @@
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+# Write your unique_english_letters function here:
+
+# Uncomment these function calls to test your function:
+#print(unique_english_letters("mississippi"))
+# should print 4
+#print(unique_english_letters("Apple"))
+# should print 4
