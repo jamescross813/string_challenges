f51209c4a2637378191022dc545432fd0dcfc50b
jamescross813 <crossjames813@gmail.com>
2023-09-18 22:22:33 -0500

    done

--- count.py
+++ count.py
@@ -5,10 +5,11 @@ def unique_english_letters(input_word):
     for let in letters:
         if input_word.find(let) !=-1:
             count +=1
+    return count
 
             
 # Uncomment these function calls to test your function:
-#print(unique_english_letters("mississippi"))
+print(unique_english_letters("mississippi"))
 # should print 4
-#print(unique_english_letters("Apple"))
+print(unique_english_letters("Apple"))
 # should print 4
