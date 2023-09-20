397bfbcaa00f891a7a8b8525fde4a778e788753d
jamescross813 <crossjames813@gmail.com>
2023-09-19 22:21:57 -0500

    for loop included

--- count_x.py
+++ count_x.py
@@ -1,6 +1,12 @@
 # Write your count_char_x function here:
 def count_char_x(word, x):
-    pass
+    counter = 0
+
+    for letter in word:
+        if letter==x:
+            counter+=1
+
+    return counter
 # Uncomment these function calls to test your tip function:
 #print(count_char_x("mississippi", "s"))
 # should print 4
