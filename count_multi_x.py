ade60f034e08fbe6c43cc88972a7f6024ed732aa
jamescross813 <crossjames813@gmail.com>
2023-09-19 22:23:59 -0500

    setup

---
+++ count_multi_x.py
@@ -0,0 +1,7 @@
+# Write your count_multi_char_x function here:
+
+# Uncomment these function calls to test your function:
+#print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+#print(count_multi_char_x("apple", "pp"))
+# should print 1
