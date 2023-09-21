ad57996a35bddabd756e76bd70847de0c4148a71
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:35:52 -0500

    function setup

--- substring.py
+++ substring.py
@@ -1,5 +1,6 @@
 # Write your substring_between_letters function here:
-
+def substring_between_letters():
+    pass
 # Uncomment these function calls to test your function:
 #print(substring_between_letters("apple", "p", "e"))
 # should print "pl"
