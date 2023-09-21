5c4fcbc9a1c2e3fc7d44a6720824fd2e2d08a1ff
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:59:51 -0500

    if statement to check if present

--- check_name.py
+++ check_name.py
@@ -2,11 +2,14 @@
 def check_for_name(sentence, name):
     lowercase_sent = sentence.lower()
     name_sent = name.lower()
-    
+    if lowercase_sent.find(name_sent) != -1:
+        return True
+    return False
+       
 # Uncomment these function calls to test your  function:
 print(check_for_name("My name is Jamie", "Jamie"))
 # should print True
-#print(check_for_name("My name is jamie", "Jamie"))
+print(check_for_name("My name is jamie", "Jamie"))
 # should print True
-#print(check_for_name("My name is Samantha", "Jamie"))
+print(check_for_name("My name is Samantha", "Jamie"))
 # should print False
