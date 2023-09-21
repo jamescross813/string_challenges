62febeb27fcf86de43edfdcda9a36d08ffd25297
jamescross813 <crossjames813@gmail.com>
2023-09-20 22:28:35 -0500

    done

--- count_multi_x.py
+++ count_multi_x.py
@@ -1,6 +1,7 @@
 # Write your count_multi_char_x function here:
 def count_multi_char_x(word, x):
-  pass
+  new_word = word.split(x)
+  return(len(new_word)-1)
 # Uncomment these function calls to test your function:
 #print(count_multi_char_x("mississippi", "iss"))
 # should print 2
