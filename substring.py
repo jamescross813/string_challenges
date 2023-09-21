27d8944f0b2ea76751b559b29e8fb76c9b775cb5
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:40:20 -0500

    done

--- substring.py
+++ substring.py
@@ -2,7 +2,9 @@
 def substring_between_letters(word, sub1, sub2):
    start_point = word.find(sub1)
    end_point = word.find(sub2)
-   
+   if start_point > -1 and end_point > -1:
+    return(word[start_point+1:end_point])
+   return word
 # Uncomment these function calls to test your function:
 #print(substring_between_letters("apple", "p", "e"))
 # should print "pl"
