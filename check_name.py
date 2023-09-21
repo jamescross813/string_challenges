4756ced5ef558236587887ad1b3481df0ccc6cd7
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:48:52 -0500

    set up

---
+++ check_name.py
@@ -0,0 +1,9 @@
+# Write your check_for_name function here:
+
+# Uncomment these function calls to test your  function:
+#print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+#print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+#print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
