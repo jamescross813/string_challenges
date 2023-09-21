2f720d36e75fa5e2a7d5895e1fa3a9c0788e97d5
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:35:22 -0500

    set up

---
+++ substring.py
@@ -0,0 +1,7 @@
+# Write your substring_between_letters function here:
+
+# Uncomment these function calls to test your function:
+#print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+#print(substring_between_letters("apple", "p", "c"))
+# should print "apple"
