6b20443e9d8d1d3e0d62e337ba4d72739d92e210
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:54:29 -0500

    lower case strings

--- check_name.py
+++ check_name.py
@@ -1,8 +1,10 @@
 # Write your check_for_name function here:
 def check_for_name(sentence, name):
-    pass
+    lowercase_sent = sentence.lower()
+    name_sent = name.lower()
+    
 # Uncomment these function calls to test your  function:
-#print(check_for_name("My name is Jamie", "Jamie"))
+print(check_for_name("My name is Jamie", "Jamie"))
 # should print True
 #print(check_for_name("My name is jamie", "Jamie"))
 # should print True
