f0a2a634546ee80559e30a85493c81d6d2f2bcc3
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:47:06 -0500

    for loop set up

--- x_length.py
+++ x_length.py
@@ -1,8 +1,11 @@
 # Write your x_length_words function here:
 def x_length_words(string, num):
-    pass
+    string_array = string.split()
+    for word in string_array:
+        pass
+
 # Uncomment these function calls to test your tip function:
-#print(x_length_words("i like apples", 2))
+print(x_length_words("i like apples", 2))
 # should print False
-#print(x_length_words("he likes apples", 2))
+print(x_length_words("he likes apples", 2))
 # should print True
