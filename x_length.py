c677142ac353ac1d5835d73ef491381c2f685c53
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:41:20 -0500

    setup

---
+++ x_length.py
@@ -0,0 +1,7 @@
+# Write your x_length_words function here:
+
+# Uncomment these function calls to test your tip function:
+#print(x_length_words("i like apples", 2))
+# should print False
+#print(x_length_words("he likes apples", 2))
+# should print True
