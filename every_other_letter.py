001a8e682bf01e222d7c2db5108c31a396b2b439
jamescross813 <crossjames813@gmail.com>
2023-09-21 16:02:41 -0500

    set up

---
+++ every_other_letter.py
@@ -0,0 +1,9 @@
+# Write your every_other_letter function here:
+
+# Uncomment these function calls to test your function:
+#print(every_other_letter("Codecademy"))
+# should print Cdcdm
+#print(every_other_letter("Hello world!"))
+# should print Hlowrd
+#print(every_other_letter(""))
+# should print 
