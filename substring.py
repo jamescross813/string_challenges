442bf8b500377a5512a9cb481954d844b3713be2
jamescross813 <crossjames813@gmail.com>
2023-09-21 15:39:10 -0500

    vars for start and end

--- substring.py
+++ substring.py
@@ -1,6 +1,8 @@
 # Write your substring_between_letters function here:
 def substring_between_letters(word, sub1, sub2):
-    pass
+   start_point = word.find(sub1)
+   end_point = word.find(sub2)
+   
 # Uncomment these function calls to test your function:
 #print(substring_between_letters("apple", "p", "e"))
 # should print "pl"
