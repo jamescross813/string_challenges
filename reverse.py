ea32f1ccab6fc218286f8769d4b523b715594eaa
jamescross813 <crossjames813@gmail.com>
2023-09-23 22:24:44 -0500

    setup

---
+++ reverse.py
@@ -0,0 +1,9 @@
+# Write your reverse_string function here:
+
+# Uncomment these function calls to test your  function:
+#print(reverse_string("Codecademy"))
+# should print ymedacedoC
+#print(reverse_string("Hello world!"))
+# should print !dlrow olleH
+#print(reverse_string(""))
+# should print
