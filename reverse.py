73eb2bb11087ea2fa47295cdb90998f0a4109f55
jamescross813 <crossjames813@gmail.com>
2023-09-23 22:26:05 -0500

    reverse string var added

--- reverse.py
+++ reverse.py
@@ -1,6 +1,6 @@
 # Write your reverse_string function here:
 def reverse_string(string):
-    pass
+    rev_string =""
 # Uncomment these function calls to test your  function:
 #print(reverse_string("Codecademy"))
 # should print ymedacedoC
