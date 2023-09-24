e2aae23ac16caf5e04d634d36e03fa4cb5441597
jamescross813 <crossjames813@gmail.com>
2023-09-23 22:28:55 -0500

    i var and return added

--- reverse.py
+++ reverse.py
@@ -1,8 +1,11 @@
 # Write your reverse_string function here:
 def reverse_string(string):
     rev_string =""
+    i=len(string)-1
+    
+    return rev_string
 # Uncomment these function calls to test your  function:
-#print(reverse_string("Codecademy"))
+# print(reverse_string("Codecademy"))
 # should print ymedacedoC
 #print(reverse_string("Hello world!"))
 # should print !dlrow olleH
