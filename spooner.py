b59af15b43cdd82c67ce28e94e00a60def444093
jamescross813 <crossjames813@gmail.com>
2023-09-25 21:51:07 -0500

    setup

---
+++ spooner.py
@@ -0,0 +1,9 @@
+# Write your make_spoonerism function here:
+
+# Uncomment these function calls to test your function:
+#print(make_spoonerism("Codecademy", "Learn"))
+# should print Lodecademy Cearn
+#print(make_spoonerism("Hello", "world!"))
+# should print wello Horld!
+#print(make_spoonerism("a", "b"))
+# should print b a
