8fffe528a81f91d3ca7f8f89a496998b9d14b1e3
jamescross813 <crossjames813@gmail.com>
2023-09-25 21:53:20 -0500

    var setup

--- spooner.py
+++ spooner.py
@@ -1,6 +1,7 @@
 # Write your make_spoonerism function here:
 def make_spoonerism(word1, word2):
-    pass
+    start1 = word1[0]+word1[1]
+    start2 = word2[0]+word2[1]
 # Uncomment these function calls to test your function:
 #print(make_spoonerism("Codecademy", "Learn"))
 # should print Lodecademy Cearn
