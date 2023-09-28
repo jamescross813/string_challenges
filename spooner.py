cfd80ad0a65313abe62b75d8896562727c6f1f65
jamescross813 <crossjames813@gmail.com>
2023-09-27 22:22:14 -0500

    done with refactored code

--- spooner.py
+++ spooner.py
@@ -1,11 +1,12 @@
 # Write your make_spoonerism function here:
 def make_spoonerism(word1, word2):
-    start1 = word1[0]
-    start2 = word2[0]
-    rest1 = word1[1:len(word1)]
-    rest2 = word2[1:len(word2)]
-    result = start2 + rest1 + " " + start1 + rest2
-    return result
+    # start1 = word1[0]
+    # start2 = word2[0]
+    # rest1 = word1[1:len(word1)]
+    # rest2 = word2[1:len(word2)]
+    # result = start2 + rest1 + " " + start1 + rest2
+    # return result
+    return word2[0] + word1[1:] + " " + word1[0] + word2[1:]
 # Uncomment these function calls to test your function:
 print(make_spoonerism("Codecademy", "Learn"))
 # should print Lodecademy Cearn
