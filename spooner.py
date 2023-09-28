8e5734042b052471569093ba0894feb32ae83de9
jamescross813 <crossjames813@gmail.com>
2023-09-27 22:20:25 -0500

    concat vars to form phrase

--- spooner.py
+++ spooner.py
@@ -4,13 +4,12 @@ def make_spoonerism(word1, word2):
     start2 = word2[0]
     rest1 = word1[1:len(word1)]
     rest2 = word2[1:len(word2)]
-    
-
+    result = start2 + rest1 + " " + start1 + rest2
+    return result
 # Uncomment these function calls to test your function:
-make_spoonerism("Codecademy", "Learn")
-# print(make_spoonerism("Codecademy", "Learn"))
+print(make_spoonerism("Codecademy", "Learn"))
 # should print Lodecademy Cearn
-#print(make_spoonerism("Hello", "world!"))
+print(make_spoonerism("Hello", "world!"))
 # should print wello Horld!
-#print(make_spoonerism("a", "b"))
+print(make_spoonerism("a", "b"))
 # should print b a
