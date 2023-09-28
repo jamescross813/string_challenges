74f6a36cef6b8c58fb50dbaf7dd8c47b68f0dfb2
jamescross813 <crossjames813@gmail.com>
2023-09-28 14:39:53 -0500

    loop abd return done

--- exclamation.py
+++ exclamation.py
@@ -1,8 +1,14 @@
 # Write your add_exclamation function here:
 def add_exclamation(string):
-    pass
+    i=0
+    phrase = string
+    while i <= 20:
+        if i>len(string):
+            phrase = phrase +"!"
+        i+=1
+    return phrase
 # Uncomment these function calls to test your function:
-#print(add_exclamation("Codecademy"))
+print(add_exclamation("Codecademy"))
 # should print Codecademy!!!!!!!!!!
 #print(add_exclamation("Codecademy is the best place to learn"))
 # should print Codecademy is the best place to learn
