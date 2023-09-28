980ab3f9e3801b9953aefceca9acfac79cd6c948
jamescross813 <crossjames813@gmail.com>
2023-09-27 22:18:33 -0500

    rest of words saved to var

--- spooner.py
+++ spooner.py
@@ -2,8 +2,13 @@
 def make_spoonerism(word1, word2):
     start1 = word1[0]
     start2 = word2[0]
+    rest1 = word1[1:len(word1)]
+    rest2 = word2[1:len(word2)]
+    
+
 # Uncomment these function calls to test your function:
-#print(make_spoonerism("Codecademy", "Learn"))
+make_spoonerism("Codecademy", "Learn")
+# print(make_spoonerism("Codecademy", "Learn"))
 # should print Lodecademy Cearn
 #print(make_spoonerism("Hello", "world!"))
 # should print wello Horld!
