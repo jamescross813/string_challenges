05492a917e60087a3dfa91a047156c415dcf9873
jamescross813 <crossjames813@gmail.com>
2023-09-27 22:22:57 -0500

    setup

---
+++ exclamation.py
@@ -0,0 +1,7 @@
+# Write your add_exclamation function here:
+
+# Uncomment these function calls to test your function:
+#print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+#print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
